7102f139841f66373daab91ea488030c2a583603
Justin Pyron <justinpyron@gmail.com>
2025-01-12 11:39:40 -0600

    Add script to scrape Google Images results

--- scrape.py
+++ scrape.py
@@ -3,7 +3,7 @@ from google_image_scraper import GoogleImageScraper
 
 df_actors = pd.read_csv("actors.csv")
 actors = df_actors["name"].tolist()
-scraper = GoogleImageScraper("images", 0.25)
+scraper = GoogleImageScraper("images", sleep_between_downloads=0.25)
 
 if __name__ == "__main__":
     scraper.scrape(actors, images_per_query=10, min_face_fraction=0.1)
