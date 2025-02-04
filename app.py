f7c1e3dd933fb4abbbb6f2d7c54fbf9d024d33a2
Justin Pyron <justinpyron@gmail.com>
2025-02-03 20:49:08 -0600

    Final touches

--- app.py
+++ app.py
@@ -4,6 +4,41 @@ from PIL import Image
 
 from retriever import Retriever
 
+WHAT_IS_THIS_APP = """
+This app uses a computer vision model to identify your celebrity doppelganger 💫
+
+#### `Data`
+I start with this [list of 1000 celebrities from IMDB](https://www.imdb.com/list/ls058011111/).
+For each celebrity, I scraped 10 images from Google Images.
+
+#### `Model`
+I use a [Hugging Face Vision Transformer model](https://huggingface.co/trpakov/vit-face-expression) trained to detect facial expressions.
+
+The model outputs an embedding vector that captures visual information from an input image.
+The original model outputs a 7-dimensional vector of logits (corresponding to 7 facial expressions).
+I modify the final linear layer of the network to output vectors of dimension 128.
+
+
+#### `Training`
+I fine-tune the model using a triplet loss function.
+Each triplet consists of three images: anchor, positive, negative.
+The triplet loss function trains the model to embed the anchor and positive images close together and the anchor and negative images far apart.
+
+I fine-fune the model in two phases:
+1. Only update weights of the final linear layer (which was freshly initialized because it was new)
+2. Update weights of all layers
+
+#### `Source code`
+See 👉 [GitHub](https://github.com/justinpyron/doppelganger-detector)
+
+#### `Next steps`
+The model is a work in progress 🙃
+
+It has weaknesses that I'd like to improve.
+For instance, the current model mostly matches to the celebrity image with the most similar clothing, hair style, and background.
+I want to explore different modeling and data processing techniques to fix this issue.
+"""
+
 
 @st.cache_resource
 def load_retriever():
@@ -11,8 +46,10 @@ def load_retriever():
 
 
 st.set_page_config(page_title="Doppelganger Detector", layout="centered", page_icon="🥸")
-st.title("Doppelganger Detector 🥸")
 doppelganger_retriever = load_retriever()
+st.title("Doppelganger Detector 🥸")
+with st.expander("What is this app?"):
+    st.markdown(WHAT_IS_THIS_APP)
 
 ready = False
 input_type_options = ["Camera", "Upload"]
@@ -23,6 +60,7 @@ input_type = st.segmented_control(
 )
 if input_type == input_type_options[0]:
     enable_camera = st.checkbox("Enable camera")
+    st.write("ℹ️ Your photo is deleted when you close the page")
     if enable_camera:
         photo_camera = st.camera_input("Take a photo", disabled=not enable_camera)
         if photo_camera:
